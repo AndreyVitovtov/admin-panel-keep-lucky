b39aacc85a56c6e6f31769ff6794ebe09caa758e
Andrii Vytovtov <andrey.vitovtov@gmail.com>
2025-05-26 20:22:06 +0300

    updated API → getUsersStats

--- app/controllers/Test.php
+++ app/controllers/Test.php
@@ -13,6 +13,35 @@ class Test extends Controller
 	public function index()
 	{
 		$api = new API();
-		echo json_encode($api->getUser(2));
+		echo json_encode($api->getUsersStats());
 	}
 }
+//admin/users-stats - этот метод не плохо бы вернуть, так как уже работал в админке
+//{
+//	"message": "Cannot GET /admin/users-stats",
+//	"error": "Not Found",
+//	"statusCode": 404
+//}
+
+//
+//admin/users-stats/4M1wRJwkQ8
+//{
+//	"total_users": 0,
+//	"total_online_users": 0
+//}
+//
+//admin/users-stats/4M1wRJwkQ8?country=France
+//{
+//	"statusCode": 500,
+//	"message": "Internal server error"
+//}
+//
+//admin/filters
+//{
+//	"country": [],
+//	"region": [],
+//	"city": [],
+//	"zip": [],
+//	"isp": [],
+//	"connectionType": []
+//}
